72afed64920e1d4df534ed07ae81e30595736b67
Mohan <mohan@hansaitechnosoft.com>
2023-05-01 16:57:51 +0530

    Initial Commit

---
+++ MapLocation/MapLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapLocationApp.swift
+//  MapLocation
+//
+//  Created by Diwakar Reddy  on 01/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MapLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
